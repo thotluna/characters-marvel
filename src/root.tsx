1b167165f4ca159af83040b2e826334a49bc4b5f
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-03 20:02:49 -0400

    style: Change bg and font-family

--- src/root.tsx
+++ src/root.tsx
@@ -19,7 +19,7 @@ export default component$(() => {
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang="en" class="bg-slate-800 text-slate-300">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
