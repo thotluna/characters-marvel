e74bae324713769a62107aff094e88f068430ee1
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-06 15:45:38 -0400

    refactor: Create a function for create hash

---
+++ src/services/get-hash.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv"
+import { MD5 } from "crypto-js"
+dotenv.config()
+
+export function getHash(){
+  const privateToken = process.env.API_TOKEN_KEY ?? ''
+  const publicToken = process.env.VITE_API_TOKEN_KEY ?? ''
+
+  const ts = new Date().getTime()
+  const hash = MD5(ts + privateToken + publicToken)
+
+  return{
+    ts,
+    hash,
+    publicToken
+  }
+}
