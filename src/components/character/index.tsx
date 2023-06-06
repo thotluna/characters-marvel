26d7936ed648c7b6f550d25af84e3adee9903df8
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-06 17:42:23 -0400

    style: repair border character

--- src/components/character/index.tsx
+++ src/components/character/index.tsx
@@ -26,7 +26,7 @@ export const CharacterComponent = component$<CharacterComponentProps>(
           onChange(character);
           onClickCharacter(character.id);
         }}
-        class="relative max-w-xs justify-self-center self-center bg-stone-950 w-full h-96 overflow-hidden rounded-t-3xl before:absolute before:top-0 before:right-0 before:w-full before:h-1/2 before:bg-red-600 before:transition-all before:duration-200  hover:before:absolute hover:before:top-0 hover:before:right-0 hover:before:w-full hover:before:h-96 hover:before:bg-red-600 after:absolute after:overflow-hidden after:bottom-0 after:right-0 after:top-auto after:border-t-transparent after:border-r-slate-950 after:border-b-transparent after:border-l-transparent   after:border-t-[12px] after:border-r-[12px] after:border-b-0 after:border-l-0 group"
+        class="relative max-w-xs justify-self-center self-center bg-stone-950 w-full h-96 overflow-hidden rounded-t-3xl before:absolute before:top-0 before:right-0 before:w-full before:h-1/2 before:bg-red-600 before:transition-all before:duration-200  hover:before:absolute hover:before:top-0 hover:before:right-0 hover:before:w-full hover:before:h-96 hover:before:bg-red-600 after:absolute after:overflow-hidden after:bottom-0 after:right-0 after:top-auto after:border-t-transparent after:border-r-slate-800 after:border-b-transparent after:border-l-transparent   after:border-t-[12px] after:border-r-[12px] after:border-b-0 after:border-l-0 group"
       >
         <header class="relative w-full h-[11.5rem] overflow-hidden transition transform duration-200 ease-linear group-hover:scale-110">
           <Image src={srcImage} alt={character.name} />
